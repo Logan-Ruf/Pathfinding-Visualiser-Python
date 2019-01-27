ed0e7669795037150b9e17ebe7eae311d1212a87
dde6khkg <nic.bassplayer@yahoo.com>
2019-01-27 15:30:08 -0800

    .gitignore test

--- classes/pathrender.py
+++ classes/pathrender.py
@@ -9,5 +9,5 @@ class PathRender:
     def pathRender(self, bestPath):
         for pos in bestPath:
             self.Grid.changeColor(pos, (0, 0, 0))
-            time.sleep(.005)
+            time.sleep(.01)
             pygame.display.update()
